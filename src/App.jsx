f106fac8e9419bcb16c8bf6f52d165235e377d0d
7ommyShelby <aryaalsaher@gmail.com>
2024-11-25 14:11:06 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import RBACDashboard from './comp/RBACDashboard';
-import RBACDashboard2 from './comp/RBACDashboard2';
+
 
 function App() {
 
   return (
     <>
       <RBACDashboard />
-      {/* <RBACDashboard2 /> */}
-
     </>
   )
 }
